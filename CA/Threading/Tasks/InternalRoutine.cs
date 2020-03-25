8094a36cc4caf73deceb23a4d81bac0bc4147e25
Devwarlt <devwarlt@loesoft.org>
2020-03-25 04:32:53 -0300

    New version 3.1.0.1
    Contains enhancements in InternalRoutine algorithm.

--- CA/Threading/Tasks/InternalRoutine.cs
+++ CA/Threading/Tasks/InternalRoutine.cs
@@ -12,7 +12,7 @@ namespace CA.Threading.Tasks
     public sealed class InternalRoutine : IDisposable
     {
         private readonly ManualResetEvent resetEvent;
-        private readonly Action<bool> routine;
+        private readonly Action<InternalRoutine, bool> routine;
         private readonly int timeout;
 
         private bool isCanceled = false;
@@ -23,7 +23,7 @@ namespace CA.Threading.Tasks
 
 #pragma warning restore CS1591
             int timeout,
-            Action routine
+            Action<InternalRoutine> routine
             ) : this(timeout, routine, null) { }
 
 #pragma warning disable CS1591
@@ -32,7 +32,7 @@ namespace CA.Threading.Tasks
 
 #pragma warning restore CS1591
             int timeout,
-            Action routine,
+            Action<InternalRoutine> routine,
             Action<string> errorLogger
             )
         {
@@ -40,7 +40,7 @@ namespace CA.Threading.Tasks
             if (routine == null) throw new ArgumentNullException("routine", "Routine couldn't be null.");
 
             this.timeout = timeout;
-            this.routine = (cancel) => { if (!cancel) routine.Invoke(); };
+            this.routine = (instance, cancel) => { if (!cancel) routine.Invoke(instance); };
 
             resetEvent = new ManualResetEvent(false);
             onError += (s, e) =>
@@ -52,6 +52,11 @@ namespace CA.Threading.Tasks
 
         private event EventHandler<Exception> onError;
 
+        /// <summary>
+        /// Check if current routine still running in background.
+        /// </summary>
+        public bool IsRunning => !isCanceled;
+
 #pragma warning disable CS1591
 
         public void Dispose() => isCanceled = true;
@@ -61,12 +66,20 @@ namespace CA.Threading.Tasks
         /// <summary>
         /// Starts the routine.
         /// </summary>
-        public void Start() => Loop();
+        public void Start()
+            => Task.Factory.StartNew(() => Loop(),
+                TaskCreationOptions.LongRunning)
+            .ContinueWith(t => onError.Invoke(null, t.Exception.InnerException),
+                TaskContinuationOptions.OnlyOnFaulted);
 
         /// <summary>
         /// Starts the routine asynchronously.
         /// </summary>
-        public void StartAsync() => LoopAsync();
+        public void StartAsync()
+            => Task.Factory.StartNew(() => LoopAsync(),
+                TaskCreationOptions.LongRunning)
+            .ContinueWith(t => onError.Invoke(null, t.Exception.InnerException),
+                TaskContinuationOptions.OnlyOnFaulted);
 
         /// <summary>
         /// Stop the routine.
@@ -75,24 +88,28 @@ namespace CA.Threading.Tasks
 
         private void Loop()
         {
-            Task.Run(() => routine.Invoke(isCanceled)).ContinueWith(t =>
+            Task.Run(() => routine.Invoke(this, isCanceled)).ContinueWith(t =>
                 onError.Invoke(null, t.Exception.InnerException),
                 TaskContinuationOptions.OnlyOnFaulted
             );
 
             if (isCanceled) return;
+
+            resetEvent.WaitOne(timeout);
 
             Loop();
         }
 
         private async void LoopAsync()
         {
-            await Task.Run(() => routine.Invoke(isCanceled)).ContinueWith(t =>
+            await Task.Run(() => routine.Invoke(this, isCanceled)).ContinueWith(t =>
                 onError.Invoke(null, t.Exception.InnerException),
                 TaskContinuationOptions.OnlyOnFaulted
             );
 
             if (isCanceled) return;
+
+            resetEvent.WaitOne(timeout);
 
             LoopAsync();
         }
