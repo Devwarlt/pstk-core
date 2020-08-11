15c7c8bf3c45a43778f0770d739cc83972880fc4
Devwarlt <devwarlt@loesoft.org>
2020-08-11 11:35:14 -0300

    Added support for missing operations (ConcurrentDictionary)

--- CA/Extensions/Concurrent/ConcurrentDictionaryExtensions.cs
+++ CA/Extensions/Concurrent/ConcurrentDictionaryExtensions.cs
@@ -11,6 +11,62 @@ namespace CA.Extensions.Concurrent
 #pragma warning restore
     {
         /// <summary>
+        /// Provides a predicate check from all <see cref="ConcurrentDictionary{TKey, TValue}"/> entries,
+        /// but rather than create a new collection, it'll do a loop through exist entries.
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="collection"></param>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        public static IEnumerable<KeyValuePair<TKey, TValue>> EntryWhere<TKey, TValue>(this ConcurrentDictionary<TKey, TValue> collection, Func<KeyValuePair<TKey, TValue>, bool> predicate)
+        {
+            foreach (var entry in collection)
+                if (predicate.Invoke(entry))
+                    yield return entry;
+        }
+
+        /// <summary>
+        /// Provides a predicate check from all <see cref="ConcurrentDictionary{TKey, TValue}"/> entries,
+        /// but rather than create a new collection, it'll do a loop through exist entries in parallel.
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="collection"></param>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        public static KeyValuePair<TKey, TValue>[] EntryWhereAsParallel<TKey, TValue>(this ConcurrentDictionary<TKey, TValue> collection, Func<KeyValuePair<TKey, TValue>, bool> predicate)
+            => collection.AsParallel().Where(_ => predicate.Invoke(_)).Select(_ => _).ToArray();
+
+        /// <summary>
+        /// Provides a predicate check from all <see cref="ConcurrentDictionary{TKey, TValue}.Keys"/> entries,
+        /// but rather than create a new collection, it'll do a loop through exist keys.
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="collection"></param>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        public static IEnumerable<TKey> KeyWhere<TKey, TValue>(this ConcurrentDictionary<TKey, TValue> collection, Predicate<TKey> predicate)
+        {
+            foreach (var entry in collection)
+                if (predicate.Invoke(entry.Key))
+                    yield return entry.Key;
+        }
+
+        /// <summary>
+        /// Provides a predicate check from all <see cref="ConcurrentDictionary{TKey, TValue}.Keys"/> entries,
+        /// but rather than create a new collection, it'll do a loop through exist keys in parallel.
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="collection"></param>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        public static TKey[] KeyWhereAsParallel<TKey, TValue>(this ConcurrentDictionary<TKey, TValue> collection, Predicate<TKey> predicate)
+            => collection.AsParallel().Where(_ => predicate.Invoke(_.Key)).Select(_ => _.Key).ToArray();
+
+        /// <summary>
         /// Provides a predicate check from all <see cref="ConcurrentDictionary{TKey, TValue}.Values"/> entries,
         /// but rather than create a new collection, it'll do a loop through exist values.
         /// </summary>
